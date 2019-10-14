116ec99d5ad078bf54324caae12bbad6c22f200a
Your Name <annah19@ru.is>
2019-10-14 22:08:54 +0000

    DONE

--- airline_seating.py
+++ airline_seating.py
@@ -49,9 +49,14 @@ def print_plane(row_list):
 
 
 def reserve_seat(row_seat,row_list,reserved_seats):
-    ''''''
+    '''Checks if the seat is already taken or if 
+        the seat number is in the input from the user. 
+        Updates the row_list and reserved_seats list'''
+
+    # We need to split the seat from the user input
     row_seat = row_seat.split()
 
+    # We make sure the row is a number not a string
     row = int(row_seat[0])
     seat = row_seat[1]
             
@@ -60,83 +65,82 @@ def reserve_seat(row_seat,row_list,reserved_seats):
 
         return
 
+    # We check if the requested row is a higher 
+    # number than the row_list (total)
+    # or if the char is not in the row
     if (len(row_list) < row or seat not in row_list[row-1]):
         print("Seat number is invalid!")
 
         return
 
-
     for i in range(0,len(row_list)):
 
+        # Check if the current row is the requested row
         if i == row-1:
+
+            # Find the seat number in the requested row
             for x in range(0,len(row_list[i])):
+                
+                # If the current seat matches the requested seat
+                # we replace the char with "X" and update the
+                # reserved_seats list 
                 if row_list[i][x] == seat:
                     row_list[i][x] = "X"
                     reserved_seats.append((row,seat))
 
-    return row_list,reserved_seats
+    return True
 
-def reserving_seats(row_list):
-    reserved_seats = []
+def reserving_seats(row_list, reserved_seats):
+    '''Ask the user for the seat number, 
+        prints out an updated plane list if seat available'''
+
     row_seat = input("Input seat number (row seat): ")
 
     reserve_seat_results = reserve_seat(row_seat,row_list,reserved_seats)
-    row_list = reserve_seat_results[0]
 
-    reserved_seats = reserve_seat_results[1]
+    # If true we print the plane
+    if reserve_seat_results:
 
-    print_plane(row_list)
+        # Print out the current status of the plane
+        print_plane(row_list)
+
+    # If false we start with the function again
+    else:
+        reserving_seats(row_list, reserved_seats)
+
+    return 
+            
+def more_seats(row_list, reserved_seats):
+    '''Allows the user to reserve more than one seat'''
     while True:
         more_seats = input("More seats (y/n)? ")
         if more_seats == "y":
-            row_seat = input("Input seat number (row seat): ")
-            reserve_seat_results = reserve_seat(row_seat,row_list,reserved_seats)
-            if reserve_seat_results:
-
-                row_list = reserve_seat_results[0]
-                reserved_seats = reserve_seat_results[1]
-                print_plane(row_list)
-
-        elif more_seats == "n":
-            break
             
+            reserving_seats(row_list, reserved_seats)
 
+        elif more_seats == 'n':
+            break
+
+    return row_list, reserved_seats
 
 def main():
+
     rows = get_number_of_rows()
     seats = get_number_of_seats()
-
+    
+    # set up the list
     row_list = data_list(rows,seats)
 
+    # print out the plane
     print_plane(row_list)
+    
+    reserved_seats = []
 
-    reserving_seats(row_list)
+    # start reserving seats
+    reserving_seats(row_list,reserved_seats)
 
-    # while True:
-    #     more_seats = input("More seats (y/n)? ")
+    # reserve more seats
+    more_seats(row_list,reserved_seats)
 
-    #     if more_seats == "y":
-    #         row_seat = input("Input seat number (row seat): ")
-    #         row_seat = row_seat.split()
-
-    #         row = int(row_seat[0])
-    #         seat = row_seat[1]
-            
-    #         if (row,seat) in reserved_seats:
-    #             print("That seat is taken!")
-    #             continue
-
-    #         if (len(row_list) < row or seat not in row_list[row]):
-    #             print("Seat number is invalid!")
-    #             continue
-
-    #         reserve_seat_results = reserve_seat(row,seat,row_list,reserved_seats)
-    #         row_list = reserve_seat_results[0]
-    #         reserved_seats = reserve_seat_results[1]
-
-    #         print_plane(row_list)
-
-        # else:
-        #     break
 
 main()
