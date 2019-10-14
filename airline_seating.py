e9a341345e87b254426b9f433cababd2994c3085
Your Name <annah19@ru.is>
2019-10-14 20:43:24 +0000

    Held þetta sé að verða komið

--- airline_seating.py
+++ airline_seating.py
@@ -1,22 +1,28 @@
-
 SEAT_COL = ['A','B','C','D','E','F','G','H','I','J']
 
 def get_number_of_rows():
     ''' returns a number of rows '''
+
     rows = int(input("Enter number of rows: "))
 
     return rows
 
 def get_number_of_seats():
     ''' returns a number of seats '''
+
     seats = int(input("Enter number of seats in each row: "))
 
     return seats
 
 def data_list(rows,seats):
+    '''Take in the row and seat from the user 
+    and return a list of list with seatnumber row-times'''
 
     row_list = []
+
     for i in range(0,rows):
+
+        # We only need the seats in the list that the user asks for
         row_list.append(SEAT_COL[:seats])
 
     return row_list
@@ -29,7 +35,7 @@ def print_plane(row_list):
     for i in range(0,len(row_list)):
         print("{:>2d}".format(i+1),end="   ")
 
-        # For every seat
+        # For every seat 
         for x in range(0, len(row_list[i])):
             print(row_list[i][x], end=" ")
 
@@ -37,17 +43,28 @@ def print_plane(row_list):
             if (len(row_list[i])-1) // 2 == x:
                 print(end=" ")
         
-
         print()
 
     return
 
-def is_seat_reserved(row,seat,row_list):
-    
-    pass
 
+def reserve_seat(row_seat,row_list,reserved_seats):
+    ''''''
+    row_seat = row_seat.split()
 
-def reserve_seat(row,seat,row_list,reserved_seats):
+    row = int(row_seat[0])
+    seat = row_seat[1]
+            
+    if (row,seat) in reserved_seats:
+        print("That seat is taken!")
+
+        return
+
+    if (len(row_list) < row or seat not in row_list[row-1]):
+        print("Seat number is invalid!")
+
+        return
+
 
     for i in range(0,len(row_list)):
 
@@ -59,10 +76,30 @@ def reserve_seat(row,seat,row_list,reserved_seats):
 
     return row_list,reserved_seats
 
+def reserving_seats(row_list):
+    reserved_seats = []
+    row_seat = input("Input seat number (row seat): ")
 
+    reserve_seat_results = reserve_seat(row_seat,row_list,reserved_seats)
+    row_list = reserve_seat_results[0]
 
+    reserved_seats = reserve_seat_results[1]
 
+    print_plane(row_list)
+    while True:
+        more_seats = input("More seats (y/n)? ")
+        if more_seats == "y":
+            row_seat = input("Input seat number (row seat): ")
+            reserve_seat_results = reserve_seat(row_seat,row_list,reserved_seats)
+            if reserve_seat_results:
 
+                row_list = reserve_seat_results[0]
+                reserved_seats = reserve_seat_results[1]
+                print_plane(row_list)
+
+        elif more_seats == "n":
+            break
+            
 
 
 def main():
@@ -70,37 +107,36 @@ def main():
     seats = get_number_of_seats()
 
     row_list = data_list(rows,seats)
-    reserved_seats = []
 
     print_plane(row_list)
-    while True:
-        more_seats = input("More seats (y/n)? ")
 
-        if more_seats == "y":
-            row_seat = input("Input seat number (row seat): ")
-            row_seat = row_seat.split()
+    reserving_seats(row_list)
 
-            row = int(row_seat[0])
-            seat = row_seat[1]
+    # while True:
+    #     more_seats = input("More seats (y/n)? ")
+
+    #     if more_seats == "y":
+    #         row_seat = input("Input seat number (row seat): ")
+    #         row_seat = row_seat.split()
+
+    #         row = int(row_seat[0])
+    #         seat = row_seat[1]
             
-            if (row,seat) in reserved_seats:
-                print("That seat is taken!")
-                continue
+    #         if (row,seat) in reserved_seats:
+    #             print("That seat is taken!")
+    #             continue
 
-            if (len(row_list) < row or seat not in row_list[row-1]):
-                print("Seat number is invalid!")
-                continue
+    #         if (len(row_list) < row or seat not in row_list[row]):
+    #             print("Seat number is invalid!")
+    #             continue
 
-            reserve_seat_results = reserve_seat(row,seat,row_list,reserved_seats)
-            row_list = reserve_seat_results[0]
-            reserved_seats = reserve_seat_results[1]
+    #         reserve_seat_results = reserve_seat(row,seat,row_list,reserved_seats)
+    #         row_list = reserve_seat_results[0]
+    #         reserved_seats = reserve_seat_results[1]
 
-            print_plane(row_list)
-                #     print("That seat is taken!")
-                # # ef sæti er ekki til
-                #     print("Seat number is invalid!")
+    #         print_plane(row_list)
 
-        else:
-            break
+        # else:
+        #     break
 
 main()
